407c1dbd3d4384ae6695babca53564aaa5afdb7f
Janith Madarasinghe <janithmadarasinghe@gmail.com>
2026-07-20 13:22:06 +0530

    feat: add scripts to start and stop the database instance

---
+++ start-db.sh
@@ -0,0 +1,2 @@
+AWS_PROFILE=ef aws rds start-db-instance \
+  --db-instance-identifier effy-dev-db --region ap-southeast-2
---
+++ stop-db.sh
@@ -0,0 +1,2 @@
+AWS_PROFILE=ef aws rds stop-db-instance \
+  --db-instance-identifier effy-dev-db --region ap-southeast-2
